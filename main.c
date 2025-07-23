60f334e22156f64e30bb9b1fa7480a90254b31f9
David Rio Deiros <driodeiros@gmail.com>
2025-07-23 15:11:35 -0400

    moving to 3d

--- main.c
+++ main.c
@@ -5,7 +5,7 @@
 #define SCREEN_HEIGHT 600
 
 typedef struct {
-    int x, y;
+    float x, y, z;
 } Point;
 
 typedef struct {
@@ -13,7 +13,7 @@ typedef struct {
 } Line;
 
 typedef struct {
-    float center_x, center_y;
+    float center_x, center_y, center_z;
     float zoom;
 } WorldCamera;
 
@@ -25,16 +25,18 @@ typedef struct {
 
 
 
-Point world_to_screen(float world_x, float world_y, WorldCamera* cam, int screen_width, int screen_height) {
+Point world_to_screen(Point world_point, WorldCamera* cam, int screen_width, int screen_height) {
     Point screen_point;
-    screen_point.x = (int)((world_x - cam->center_x) * cam->zoom + screen_width / 2);
-    screen_point.y = (int)((world_y - cam->center_y) * cam->zoom + screen_height / 2);
+    screen_point.x = (world_point.x - cam->center_x) * cam->zoom + screen_width / 2;
+    screen_point.y = (world_point.y - cam->center_y) * cam->zoom + screen_height / 2;
+    screen_point.z = (world_point.z - cam->center_z) * cam->zoom;
     return screen_point;
 }
 
 void draw_image(WorldImage* img, WorldCamera* camera) {
     // where do the (x,y) coordinate of our image in our world map in the screen?
-    Point screen_pos = world_to_screen(img->x, img->y, camera, SCREEN_WIDTH, SCREEN_HEIGHT);
+    Point world_pos = {img->x, img->y, 0.0f};
+    Point screen_pos = world_to_screen(world_pos, camera, SCREEN_WIDTH, SCREEN_HEIGHT);
     
     // Same, but how big the image should be in our screen?
     float screen_width = img->width * camera->zoom;
@@ -54,24 +56,24 @@ void draw_scene(WorldCamera* camera) {
     ClearBackground(BLACK);
 
     Point points[] = {
-        {0, 0}, {2, 0}, {2, 2}, {0, 2}
+        {0, 0, 0}, {2, 0, 0}, {2, 2, 0}, {0, 2, 0}
     };
 
     Line lines[] = {
-        {{0, 0}, {2, 0}},
-        {{2, 0}, {2, 2}},
-        {{2, 2}, {0, 2}},
-        {{0, 2}, {0, 0}}
+        {{0, 0, 0}, {2, 0, 0}},
+        {{2, 0, 0}, {2, 2, 0}},
+        {{2, 2, 0}, {0, 2, 0}},
+        {{0, 2, 0}, {0, 0, 0}}
     };
 
     for (int i = 0; i < 4; i++) {
-        Point screen_point = world_to_screen(points[i].x, points[i].y, camera, SCREEN_WIDTH, SCREEN_HEIGHT);
+        Point screen_point = world_to_screen(points[i], camera, SCREEN_WIDTH, SCREEN_HEIGHT);
         DrawCircle(screen_point.x, screen_point.y, 3, WHITE);
     }
 
     for (int i = 0; i < 4; i++) {
-        Point start = world_to_screen(lines[i].start.x, lines[i].start.y, camera, SCREEN_WIDTH, SCREEN_HEIGHT);
-        Point end = world_to_screen(lines[i].end.x, lines[i].end.y, camera, SCREEN_WIDTH, SCREEN_HEIGHT);
+        Point start = world_to_screen(lines[i].start, camera, SCREEN_WIDTH, SCREEN_HEIGHT);
+        Point end = world_to_screen(lines[i].end, camera, SCREEN_WIDTH, SCREEN_HEIGHT);
         DrawLine(start.x, start.y, end.x, end.y, WHITE);
     }
 
@@ -79,8 +81,8 @@ void draw_scene(WorldCamera* camera) {
     DrawLine(SCREEN_WIDTH / 2, 0, SCREEN_WIDTH / 2, SCREEN_HEIGHT, WHITE);
 
     char buf[128];
-    snprintf(buf, sizeof(buf), "cam.x=%.2f cam.y=%.2f zoom=%.2f",
-             camera->center_x, camera->center_y, camera->zoom);
+    snprintf(buf, sizeof(buf), "cam.x=%.2f cam.y=%.2f cam.z=%.2f zoom=%.2f",
+             camera->center_x, camera->center_y, camera->center_z, camera->zoom);
     DrawText(buf, 10, 20, 20, WHITE);
 }
 
@@ -88,6 +90,7 @@ int main() {
     WorldCamera camera;
     camera.center_x = 1.0f;
     camera.center_y = 1.0f;
+    camera.center_z = 0.0f;
     camera.zoom = 100.0f;
 
 
